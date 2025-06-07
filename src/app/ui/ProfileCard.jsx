8f7729e6cda3a7e13ec9171eae33787060ff41a2
Edwin Kim <edwinkim@Edwins-MacBook-Air.local>
2025-06-07 18:40:14 -0400

    added ProfileCard component, progress on about page

--- src/app/ui/ProfileCard.jsx
+++ src/app/ui/ProfileCard.jsx
@@ -1,14 +1,12 @@
-const ProfileCard = ({icon, title, description}) => {
+const ProfileCard = ({imageUrl, name, title}) => {
   return (
-    <>
-      <div className="flex p-4 bg-white rounded-lg shadow-md border border-solid border-gray-300">
-
-        <div className="pl-4">
-          <h1 className="text-lg font-bold">{title}</h1>
-          <p className="text-sm text-gray-500 font-tajawal">{description}</p>
-        </div>
+    <div className="m-8 p-4 flex flex-wrap justify-center text-center gap-4 bg-white rounded-lg shadow-md border border-solid border-gray-300 max-w-xs">
+      <div className="pl-4">
+        <img src={imageUrl} alt={name} className="w-40 h-40 rounded-lg mb-2" />
+        <h2 className="text-lg font-bold font-tajawal">{name}</h2>
+        <p className="text-sm text-gray-500 font-tajawal">{title}</p>
       </div>
-    </>
+    </div>
   );
 };
 
