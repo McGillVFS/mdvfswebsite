5be5d4a855677c5c11e63cc154815f8b9ac092e7
Edwin Kim <edwinkim@Edwins-MacBook-Air.local>
2025-06-07 17:14:28 -0400

    altered title component, added profilecard component

--- src/app/about/page.jsx
+++ src/app/about/page.jsx
@@ -1,17 +1,23 @@
-"use client"
+"use client";
 
-import { useState } from "react"
-import { useRouter } from 'next/navigation'
-import Title from "../ui/Title"
-import SubHeading from "../ui/SubHeading"
+import { useState } from "react";
+import { useRouter } from "next/navigation";
+import Title from "../ui/Title";
+import SubHeading from "../ui/SubHeading";
+import ProfileCard from "../ui/ProfileCard";
 
 export default function About() {
-    
-    return (
-        <div>
-            <Title text={"About Us"}/>
-            <SubHeading text={"WHO WE ARE"}/>
-        </div>
-    )
-
+  return (
+    <div>
+      <Title text={"ABOUT US"} />
+      <SubHeading text={"WHO WE ARE"} />
+      <SubHeading text={"OUR TEAM"} />
+      <SubHeading text={"AWARDS AND ACHIEVEMENTS"} />
+      <SubHeading text={"KEY DATES"} />
+      <ProfileCard
+        title={"Mentoring"}
+        description={"Each new member is assigned a project lead, where they can be mentored to learn the technical aspects of each project. You’ll have the opportunity to work with other new members of the team and socialize with people with the same interests as you!"}
+      />
+    </div>
+  );
 }
