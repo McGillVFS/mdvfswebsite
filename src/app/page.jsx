091191d2d4c7a99e88380e0ea281c73b0da3cc8c
“David <barddro.art@gmail.com>
2025-08-10 16:32:35 -0400

    initial

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -4,12 +4,18 @@ import { useState } from "react"
 import { useRouter } from 'next/navigation'
 import Navbar from "./ui/Navbar"
 import Footer from "./ui/Footer"
+import ScrollVideo from "./ui/ScrollVideo"
 
 
 export default function Home() {
     
     return (
         <div>
+            <ScrollVideo />
+
+            <div className='bg-black'>
+                <p className="text-red-500">Our Work</p>
+            </div>
             
         </div>
     )
