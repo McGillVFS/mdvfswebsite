7263e12ae25c7a0fedba514e9c7a9f13f8c75f38
“David <barddro.art@gmail.com>
2025-08-26 01:24:43 -0400

    STILL NOT WORKING

--- src/app/ui/3DModelPopup.jsx
+++ src/app/ui/3DModelPopup.jsx
@@ -7,15 +7,15 @@ const ModelViewer = dynamic(() => import("./ModelViewer"), {
 });
 
 const ThreeDModelPopup = ({ model_path, setIsOpen }) => {
-    
-
     return(
         <div className="fixed inset-0 z-50 grid place-items-center bg-black/50">
             <div className="relative w-11/12 max-w-4xl rounded-lg bg-black border-white border-2 p-4">
                 <div>
-                    <button onClick={() => setIsOpen(false)}> <IoMdClose className="text-white size-8 float-left" /></button>
+                    <button onClick={() => setIsOpen(false)}> 
+                        <IoMdClose className="text-white w-8 h-8 float-left" />
+                    </button>
                     <ModelViewer
-                        src={model_path}
+                        src={model_path}  // Use the actual prop
                         alt="3D model"
                         camera-controls
                         auto-rotate
