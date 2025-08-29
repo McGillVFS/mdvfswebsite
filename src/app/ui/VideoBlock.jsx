ce25461b50ab2a4391287f64ec77a0b9cd165d53
Edwin Kim <edwinkim@mac.home>
2025-08-29 13:44:08 -0400

    added gradient to home page

--- src/app/ui/VideoBlock.jsx
+++ src/app/ui/VideoBlock.jsx
@@ -10,6 +10,7 @@ export default function VideoBlock({ src }) {
         playsInline
         className="absolute top-0 left-0 w-full h-full object-cover"
       />
+      <div className="absolute top-140 left-0 w-full h-60 bg-gradient-to-b from-black/0 to-red-500/60" />
     </div>
   )
 }
