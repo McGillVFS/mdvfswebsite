b486e4db9dc36be24c7ac4ed71b6861c55f5da0f
edahki <edwinkim704@gmail.com>
2025-11-28 16:58:43 -0500

    added hackathon links

--- src/app/hackathon/page.jsx
+++ src/app/hackathon/page.jsx
@@ -19,11 +19,11 @@ export default function Hackathon() {
           <SubHeading text={"AeroHacks"} />
 
           <div className="px-4">
-            <p className="text-black-500 text-xl font-tajawal pb-1">Apply to AeroHacks!</p>
+            <p className="text-black-500 text-xl font-tajawal pb-1">Join 150+ students for an exciting 3-day challenge in drone programming, computer vision, embedded systems, and autonomous navigation. Code, collaborate, and compete using real ESP32-powered drones.</p>
           </div>
 
           <div className="flex items-center justify-center py-8">
-              <button onClick={() => window.open("https://docs.google.com/forms/d/1zDdhk-TNLQL2yqBGmVQZ_d1SJZEsFiQGE-rcQCcJGLU/viewform?edit_requested=true", "_blank")} className="bg-red-500 hover:bg-red-600 text-white px-4 rounded-xl h-14 min-w-11/12 cursor-pointer">Apply Now!</button>
+              <button onClick={() => window.open("https://www.mcgillaerohacks.com/", "_blank")} className="bg-red-500 hover:bg-red-600 text-white px-4 rounded-xl h-14 min-w-11/12 cursor-pointer">Hackathon Website</button>
           </div>
 
         </div>
